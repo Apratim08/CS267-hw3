a527813728cdb566266bec7ca94c8f9b43ec60ef
Apratim08 <apratimbanerjee08@gmail.com>
2024-04-10 15:38:12 -0700

    opt_v1

--- build/CMakeFiles/kmer_hash_19.dir/kmer_hash.cpp.o.d
+++ build/CMakeFiles/kmer_hash_19.dir/kmer_hash.cpp.o.d
@@ -258,7 +258,26 @@ CMakeFiles/kmer_hash_19.dir/kmer_hash.cpp.o: \
  /global/homes/a/apratim/CS267-hw3/kmer_t.hpp \
  /global/homes/a/apratim/CS267-hw3/packing.hpp \
  /usr/include/c++/12/cassert /usr/include/assert.h \
- /global/homes/a/apratim/CS267-hw3/pkmer_t.hpp \
+ /global/homes/a/apratim/CS267-hw3/pkmer_t.hpp /usr/include/c++/12/math.h \
+ /usr/include/c++/12/cmath /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h /usr/include/bits/mathinline.h \
+ /usr/include/c++/12/bits/specfun.h /usr/include/c++/12/tr1/gamma.tcc \
+ /usr/include/c++/12/tr1/special_function_util.h \
+ /usr/include/c++/12/tr1/bessel_function.tcc \
+ /usr/include/c++/12/tr1/beta_function.tcc \
+ /usr/include/c++/12/tr1/ell_integral.tcc \
+ /usr/include/c++/12/tr1/exp_integral.tcc \
+ /usr/include/c++/12/tr1/hypergeometric.tcc \
+ /usr/include/c++/12/tr1/legendre_function.tcc \
+ /usr/include/c++/12/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/12/tr1/poly_hermite.tcc \
+ /usr/include/c++/12/tr1/poly_laguerre.tcc \
+ /usr/include/c++/12/tr1/riemann_zeta.tcc \
  /global/homes/a/apratim/CS267-hw3/read_kmers.hpp \
  /usr/include/c++/12/fstream \
  /usr/include/c++/12/x86_64-suse-linux/bits/basic_file.h \
--- hash_map.hpp
+++ hash_map.hpp
@@ -2,70 +2,58 @@
 
 #include "kmer_t.hpp"
 #include <upcxx/upcxx.hpp>
-
+#include <math.h>
 
 
 struct HashMap {
-
-    // Upcxx data structures 
-    std::vector< upcxx::global_ptr<int> > used; // used array global pointer
-    std::vector< upcxx::global_ptr<kmer_pair> > data; // data array global pointer
-    size_t size_of_chunks; // segmentation size for each thread
-    upcxx::atomic_domain<int32_t>* ad; // atomic domain class
-
+    using GlobalPtrsToKmer = std::vector<upcxx::global_ptr<kmer_pair>>;
+    using GlobalPtrsToInt = std::vector<upcxx::global_ptr<int>>;
+    
+    GlobalPtrsToKmer data;
+    GlobalPtrsToInt used;
+	
     size_t my_size;
-
-    size_t size() const noexcept;
+	
+    size_t globalHashTableSize;
+	
+	size_t size() const noexcept;
+	
+    size_t totalGlobalSize() const noexcept;
 
     HashMap(size_t size);
-    ~HashMap();
 
-    // Most important functions: insert and retrieve
-    // k-mers from the hash table.
     bool insert(const kmer_pair& kmer);
     bool find(const pkmer_t& key_kmer, kmer_pair& val_kmer);
-
-    // Helper functions
-
-    // Write and read to a logical data slot in the table.
     void write_slot(uint64_t slot, const kmer_pair& kmer);
     kmer_pair read_slot(uint64_t slot);
-
-    // Request a slot or check if it's already used.
     bool request_slot(uint64_t slot);
     bool slot_used(uint64_t slot);
+
+private: 
+    upcxx::atomic_domain<int> atomicDomain;
 };
 
-HashMap::~HashMap() {
-    delete ad;
-}
-
-HashMap::HashMap(size_t size) {
-    my_size = size;
-    
-    // calculate starting index for each rank to store data
-    size_of_chunks = (my_size + upcxx::rank_n() - 1) / upcxx::rank_n();
-    data.resize(upcxx::rank_n());
+HashMap::HashMap(size_t size)
+    : globalHashTableSize(size),
+	
+      atomicDomain(upcxx::atomic_domain<int>({upcxx::atomic_op::fetch_add})) 
+{	
+	double tempSize = 
+	static_cast<double>(globalHashTableSize) / static_cast<double>(upcxx::rank_n());
+	
+    my_size = std::ceil(tempSize);
+	
     used.resize(upcxx::rank_n());
-
-    // create new ad for atomic exchange 
-    ad = new upcxx::atomic_domain<int32_t>({upcxx::atomic_op::compare_exchange});
-    // initialize data and used arrays
-    size_t start_idx = upcxx::rank_me() * size_of_chunks;
-    size_t end_idx = std::min(start_idx + size_of_chunks, my_size);
-    data[upcxx::rank_me()] = upcxx::new_array<kmer_pair>(end_idx - start_idx);
-    used[upcxx::rank_me()] = upcxx::new_array<int>(end_idx - start_idx);
-    // broadcast arrays to all ranks
-    for (int i = 0; i < upcxx::rank_n(); i++) {
-        data[i] = upcxx::broadcast(data[i], i).wait();
+    data.resize(upcxx::rank_n());
+	
+    for (int i = 0; i < upcxx::rank_n(); ++i) {
+        if (i == upcxx::rank_me()) {
+            used[i] = upcxx::new_array<int>(my_size);
+            data[i] = upcxx::new_array<kmer_pair>(my_size);
+        }
         used[i] = upcxx::broadcast(used[i], i).wait();
+        data[i] = upcxx::broadcast(data[i], i).wait();       
     }
-    // local pointer to the used array
-    int* used_local = used[upcxx::rank_me()].local();
-    size_t start_local = upcxx::rank_me() * size_of_chunks;
-    size_t end_local = std::min(start_local + size_of_chunks, my_size);
-    // fill the local used array with zeros
-    std::fill_n(used_local, end_local - start_local, 0); 
 }
 
 bool HashMap::insert(const kmer_pair& kmer) {
@@ -73,88 +61,54 @@ bool HashMap::insert(const kmer_pair& kmer) {
     uint64_t probe = 0;
     bool success = false;
     do {
-        uint64_t slot = (hash + probe++) % size();
-        success = request_slot(slot);
+        uint64_t slot = (hash + probe) % totalGlobalSize();
+        success = request_slot(slot); 
         if (success) {
             write_slot(slot, kmer);
         }
-    } while (!success && probe < size());
+        ++probe;
+    } while (!success && probe < totalGlobalSize());
     return success;
 }
 
-// TODO: haven`t implemented the find function yet
 bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
     uint64_t hash = key_kmer.hash();
-    uint64_t probe = 0;
+    uint64_t probe = 0; 
     bool success = false;
     do {
-        uint64_t slot = (hash + probe++) % size();
-        int rank = slot / size_of_chunks; 
-        if (upcxx::rank_me() == rank) { // check if the slot is in the current rank
-            int index = slot % size_of_chunks; 
-            if (slot_used(slot)) {
-                val_kmer = read_slot(slot);
-                if (val_kmer.kmer == key_kmer) {
-                    success = true;
-                }
+        uint64_t slot = (hash + probe) % totalGlobalSize(); 
+        if (slot_used(slot)) {
+            val_kmer = read_slot(slot);
+            if (val_kmer.kmer == key_kmer) {
+                success = true; 
             }
         }
-    } while (!success && probe < size());
+        ++probe; 
+    } while (!success && probe < totalGlobalSize()); 
     return success;
 }
 
 bool HashMap::slot_used(uint64_t slot) { 
-    int rank = slot / size_of_chunks;
-    int index = slot % size_of_chunks;
-    return upcxx::rget(used[rank] + index).wait() != 0;
- }
+    auto ptrToUsed = used[slot / size()] + slot % size();
+    return upcxx::rget(ptrToUsed).wait() != 0; 
+}
 
 void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) { 
-    int rank = slot / size_of_chunks;
-    int index = slot % size_of_chunks;
-    return upcxx::rput(kmer, data[rank] + index).wait();
+    auto ptrToData = data[slot / size()] + slot % size();
+    upcxx::rput(kmer, ptrToData).wait(); 
 }
 
 kmer_pair HashMap::read_slot(uint64_t slot) { 
-    int rank = slot / size_of_chunks;
-    int index = slot % size_of_chunks;
-    return upcxx::rget(data[rank] + index).wait(); 
+    auto ptrToData = data[slot / size()] + slot % size();
+    return upcxx::rget(ptrToData).wait(); 
 }
 
-bool HashMap::request_slot(uint64_t slot) {
-    // if (used[slot] != 0) {
-    //     return false;
-    // } else {
-    //     used[slot] = 1;
-    //     return true;
-    // }
-    int rank = slot / size_of_chunks;
-    int index = slot % size_of_chunks;
-    // Atomically check and update the value of used[rank] + index
-    int expected = 0;
-    int desired = 1;
-    int current_value = ad->compare_exchange(used[rank] + index, expected, desired, std::memory_order_relaxed).wait();
-
-    // upcxx::global_ptr<int> ptr = &used[rank] + index; // Pointer to the memory location
-    // int expected = 0; // Expected value
-    // int desired = 1; // Desired value
-    // int* original_value_ptr = nullptr; // Pointer to store the original value
-    // upcxx::atomic_domain<int>::VALUE_RTYPE<Cxs> result = ad->compare_exchange(ptr, expected, desired, original_value_ptr, std::memory_order_relaxed).wait();
-
-    
-    // int* ptr = &used[rank] + index; // Pointer to the memory location
-    // int expected = 0; // Expected value
-    // int desired = 1; // Desired value
-    // upcxx::atomic_domain<int>::VALUE_RTYPE<Cxs> result = ad->compare_exchange(ptr, expected, desired, ).wait();
-
-
-
-    // If the previous value was not 0, the slot was already used
-    if (current_value != 0) {
-        return false;
-    } else {
-        return true;
-    }
+bool HashMap::request_slot(uint64_t slot) { 
+    auto ptrToUsed = used[slot / size()] + slot % size();
+    return atomicDomain.fetch_add(ptrToUsed, 1, std::memory_order_relaxed).wait() == 0; 
 }
+
 
 size_t HashMap::size() const noexcept { return my_size; }
+
+size_t HashMap::totalGlobalSize() const noexcept { return globalHashTableSize; }
